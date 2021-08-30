bba6b0d6474a92693e838d093fc591b6088044ab
mtndaghyani <matin.daghyani@gmail.com>
2021-08-30 12:18:59 +0430

    Add db context and models

---
+++ Backend/ETLLibrary/Database/EtlContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ETLLibrary.Database
+{
+    public class EtlContext: DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        private const string ConnectionString = "Server=.; Database=ETLDB; Trusted_Connection=True;";
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.EnableSensitiveDataLogging();
+            optionsBuilder.UseSqlServer(ConnectionString);
+        }
+    }
+}
--- Backend/ETLLibrary/Database/User.cs
+++ Backend/ETLLibrary/Database/User.cs
@@ -1,7 +1,17 @@
-﻿namespace ETLLibrary.Database
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data.Common;
+
+namespace ETLLibrary.Database
 {
     public class User
     {
+        [Key]
+        public int Id { get; set; }
         
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
     }
 }

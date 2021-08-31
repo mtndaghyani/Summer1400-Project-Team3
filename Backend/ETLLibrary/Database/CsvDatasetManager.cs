eb74a5f701f481a3b8c670e46d2f26aa0cbd32c3
Ali Nazari <X.LordAliX@yahoo.com>
2021-08-31 02:15:42 -0700

    Implement csv upload api

---
+++ Backend/ETLLibrary/Database/CsvDatasetManager.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+
+namespace ETLLibrary.Database
+{
+    public class CsvDatasetManager
+    {
+        private ConnectorMapper _mapper;
+        private const string Path = "csvFiles";
+
+        public CsvDatasetManager(EtlContext context)
+        {
+            _mapper = new ConnectorMapper(context);
+        }
+        
+        public void SaveCsv(Stream stream, string username, string fileName, long fileLength)
+        {
+            EnsureDirectoryCreated(Path);
+            EnsureUserDirectoryCreated(Path, username);
+
+            if (!File.Exists(Path + "/" + username + "/" + fileName))
+            {
+                _mapper.Add(username, fileName);
+                _mapper.Save();
+
+                string content = "";
+                int c = 0;
+                byte[] bytes = new byte[fileLength];
+                stream.Read(bytes);
+                foreach (var b in bytes)
+                {
+                    content += Convert.ToChar(b);
+                }
+
+                File.WriteAllText(Path + "/" + username + "/" + fileName, content);
+            }
+        }
+
+        private void EnsureDirectoryCreated(string path)
+        {
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+        }
+        
+        private void EnsureUserDirectoryCreated(string path, string username)
+        {
+            if (!Directory.Exists(path+"/"+username))
+            {
+                Directory.CreateDirectory(path+"/"+username);
+            }
+        }
+    }
+}
---
+++ Backend/ETLWebApp/Controllers/CsvDatasetController.cs
@@ -0,0 +1,28 @@
+﻿using ETLLibrary.Authentication;
+using ETLLibrary.Database;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETLWebApp.Controllers
+{
+    [ApiController]
+    [Route("dataset/csv")]
+    public class CsvDatasetController : Controller
+    {
+        private EtlContext _context;
+        private CsvDatasetManager _manager;
+
+        public CsvDatasetController(EtlContext context)
+        {
+            _context = context;
+            _manager = new CsvDatasetManager(_context);
+        }
+        
+        [HttpPost("/create")]
+        public void Create([FromBody] IFormFile myFile, [FromBody] string token)
+        {
+            User user = Authenticator.Tokens[token];
+            _manager.SaveCsv(myFile.OpenReadStream(), user.Username, myFile.FileName, myFile.Length);
+        }
+    }
+}

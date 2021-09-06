d6143d8d59f6034f8b1444a80500bc9cf12d3bed
mtndaghyani <matin.daghyani@gmail.com>
2021-09-06 16:29:22 +0430

    Add Serializers

---
+++ Backend/ETLLibrary/CsvSerializer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using ETLLibrary.Database.Utils;
+
+namespace ETLLibrary
+{
+    public class CsvSerializer
+    {
+        public List<List<string>> Serialize(CsvInfo info)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

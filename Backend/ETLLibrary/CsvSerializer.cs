f0289895b591dfa1fc1b0a034694662d92d01058
mtndaghyani <matin.daghyani@gmail.com>
2021-09-06 17:54:02 +0430

    Add default header if needed

--- Backend/ETLLibrary/CsvSerializer.cs
+++ Backend/ETLLibrary/CsvSerializer.cs
@@ -2,19 +2,42 @@
 using System.IO;
 using System.Linq;
 using ETLLibrary.Database.Models;
-using ETLLibrary.Database.Utils;
 using ETLLibrary.Interfaces;
 
 namespace ETLLibrary
 {
-    public class CsvSerializer: ICsvSerializer
+    public class CsvSerializer : ICsvSerializer
     {
         public List<List<string>> Serialize(Csv csv, string path)
         {
             var raw = File.ReadAllText(path);
-            return raw.Split(csv.RowDelimiter)
+            var data = raw.Split(csv.RowDelimiter)
                 .Select(row => new List<string>(row.Split(csv.ColDelimiter)))
                 .ToList();
+            if (csv.HasHeader)
+            {
+                return data;
+            }
+
+            var result = new List<List<string>> {GetDefaultHeaders(GetRowLength(data))};
+            result.AddRange(data);
+            return result;
+        }
+
+        private int GetRowLength(List<List<string>> data)
+        {
+            return data[0].Count;
+        }
+
+        private List<string> GetDefaultHeaders(int length)
+        {
+            var headers = new List<string>();
+            for (int i = 0; i < length; i++)
+            {
+                headers.Add($"field{i}");
+            }
+
+            return headers;
         }
     }
 }

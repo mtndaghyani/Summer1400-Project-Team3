cb11ac2d7b6e18555d85aabcd273ea20d6a134e1
Ali Nazari <X.LordAliX@yahoo.com>
2021-09-06 07:29:22 -0700

    Add getContent for sqlServerDataset

--- Backend/ETLLibrary/SqlServerSerializer.cs
+++ Backend/ETLLibrary/SqlServerSerializer.cs
@@ -1,7 +1,7 @@
-﻿using System.Collections.Generic;
-using ETLLibrary.Database;
+﻿using System;
+using System.Collections.Generic;
 using ETLLibrary.Database.Utils;
-using NLog.Targets;
+using Microsoft.Data.SqlClient;
 
 namespace ETLLibrary
 {
@@ -9,7 +9,49 @@ namespace ETLLibrary
     {
         public List<List<string>> Serialize(DatasetInfo info)
         {
-            throw new System.NotImplementedException();
+            var queryString = $"SELECT * FROM {info.Table};";
+            var connectionString =
+                DatabaseConfigurator.GetConnectionString(info.DbName, info.DbUsername, info.DbPassword, info.Url);
+            var connection = new SqlConnection(connectionString);
+            var command = new SqlCommand(queryString, connection);
+            try
+            {
+                command.Connection.Open();
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+            var reader = command.ExecuteReader();
+
+
+            return GetContent(reader);
+        }
+
+        public List<List<string>> GetContent(SqlDataReader reader)
+        {
+            var result = new List<List<string>>();
+            
+            var columns = new List<string>();
+            for(var i=0;i<reader.FieldCount;i++)
+            {
+                columns.Add(reader.GetName(i));
+            }
+
+            result.Add(columns);
+            
+            while (reader.Read())
+            {
+                var row = new List<string>();
+                for (int i = 0; i < reader.FieldCount; ++i)
+                {
+                    row.Add((reader.GetValue(i)).ToString());
+                }
+
+                result.Add(row);
+            }
+
+            return result;
         }
     }
 }

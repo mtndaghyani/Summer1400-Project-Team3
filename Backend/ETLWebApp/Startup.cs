e26c0122381964f85b18ec390c7adb75af3d9215
mtndaghyani <matin.daghyani@gmail.com>
2021-09-08 19:06:35 +0430

    Enable CORS access

--- Backend/ETLWebApp/Startup.cs
+++ Backend/ETLWebApp/Startup.cs
@@ -35,6 +35,15 @@ namespace ETLWebApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy(
+                    "CorsPolicy",
+                    builder => builder.WithOrigins("http://localhost:4200", "https://localhost:5001")
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .AllowCredentials());
+            });
             services.AddControllers();
             services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo {Title = "ETLWebApp", Version = "v1"}); });
             services.AddDbContext<EtlContext>();
@@ -49,12 +58,15 @@ namespace ETLWebApp
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors("CorsPolicy");
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ETLWebApp v1"));
             }
+
 
             app.UseHttpsRedirection();
 

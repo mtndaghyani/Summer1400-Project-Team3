a23b6de96a7d655b4e055bbb9103bc54d1b23609
mtndaghyani <matin.daghyani@gmail.com>
2021-09-09 10:00:36 +0430

    Fix newline row delimiter problem

--- Backend/ETLLibrary/CsvSerializer.cs
+++ Backend/ETLLibrary/CsvSerializer.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using ETLLibrary.Database.Models;
@@ -11,7 +12,8 @@ namespace ETLLibrary
         public List<List<string>> Serialize(Csv csv, string path)
         {
             var raw = File.ReadAllText(path);
-            var data = raw.Split(csv.RowDelimiter)
+            var rowDelimiter = GetRowDelimiter(csv);
+            var data = raw.Split(rowDelimiter, StringSplitOptions.None)
                 .Select(row => new List<string>(row.Split(csv.ColDelimiter)))
                 .ToList();
             if (csv.HasHeader)
@@ -22,6 +24,13 @@ namespace ETLLibrary
             var result = new List<List<string>> {GetDefaultHeaders(GetRowLength(data))};
             result.AddRange(data);
             return result;
+        }
+
+        private string[] GetRowDelimiter(Csv csv)
+        {
+            return csv.RowDelimiter.Equals("newline")
+                ? new[] {"\r\n", "\r", "\n"}
+                : new[] {csv.RowDelimiter};
         }
 
         private int GetRowLength(List<List<string>> data)

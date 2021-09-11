4c44c6f193215fb55cdd21e588713a379b353f83
Ali Nazari <X.LordAliX@yahoo.com>
2021-09-10 23:55:49 -0700

    Validate email for register

--- Backend/ETLWebApp/Models/AuthenticationModels/RegisterModel.cs
+++ Backend/ETLWebApp/Models/AuthenticationModels/RegisterModel.cs
@@ -1,8 +1,11 @@
-﻿namespace ETLWebApp.Models.AuthenticationModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ETLWebApp.Models.AuthenticationModels
 {
     public class RegisterModel
     {
         public string Username { get; set; }
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
         public string Email { get; set; }
         public string Password { get; set; }
         public string FirstName { get; set; }
